cc84ddab6fae32bb3c76d3dc4efb4299480ab920
kovacsalex <kovacsalex95@gmail.com>
2021-07-06 22:35:00 +0200

    functions to get atmosphere and its radius

--- Assets/Environment/_Scripts/World.cs
+++ Assets/Environment/_Scripts/World.cs
@@ -6,6 +6,7 @@ using lxkvcs.PlanetGen;
 public class World : MonoBehaviour
 {
     Planet planet = null;
+    PlanetAtmosphereScaler atmosphere = null;
 
     public Planet getPlanet()
     {
@@ -13,5 +14,22 @@ public class World : MonoBehaviour
             planet = gameObject.GetComponentInChildren<Planet>();
 
         return planet;
+    }
+
+    public PlanetAtmosphereScaler getAtmosphere()
+    {
+        if (atmosphere == null)
+            atmosphere = gameObject.GetComponentInChildren<PlanetAtmosphereScaler>();
+
+        return atmosphere;
+    }
+
+    public float getAtmosphereRadius()
+    {
+        PlanetAtmosphereScaler atmo = getAtmosphere();
+        if (atmo == null)
+            return 0;
+
+        return atmo.transform.localScale.x * 0.5f;
     }
 }
