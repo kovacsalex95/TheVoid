4f1dd032e249524dbde958a2a06767e673d03e8a
Alex Kovács <kovacsalex95@gmail.com>
2021-08-13 20:43:00 +0200

    Cleared for now

--- Assets/Misc/_Scripts/GameController.cs
+++ Assets/Misc/_Scripts/GameController.cs
@@ -5,23 +5,5 @@ using UnityEngine.InputSystem;
 
 public class GameController : MonoBehaviour
 {
-    [System.NonSerialized]
-    public PlayerInput input = null;
-
-    public TMPro.TextMeshProUGUI FpsCounter;
-
-    World closestWorld = null;
-
-    void Awake()
-    {
-        input = GetComponent<PlayerInput>();
-
-        closestWorld = Util.getClosestWorld(Camera.main.transform.position);
-    }
-
-    private void Update()
-    {
-        if (FpsCounter)
-            FpsCounter.SetText(string.Format("{0} FPS", Mathf.RoundToInt(1f / Time.deltaTime)));
-    }
+    
 }
