bb5f47cfb0fa17751664ba89f07376e0b9d22c53
Alex Kovács <kovacsalex95@gmail.com>
2021-08-13 20:42:49 +0200

    CalcUtil implemented for float and vector calculations

---
+++ Assets/Misc/_Scripts/CalcUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Misc._Scripts
+{
+    class CalcUtil
+    {
+        public static float SmoothDamp(float current, float target, float time = 0.1f, float thresholdRatio = 0.01f)
+        {
+            if (Mathf.Abs(target - current) <= target * thresholdRatio)
+                return target;
+
+            float vel = 0;
+            return Mathf.SmoothDamp(current, target, ref vel, time);
+        }
+        public static Vector3 SmoothDamp(Vector3 current, Vector3 target, float time = 0.1f, float absoluteThreshold = 0.01f)
+        {
+            if ((current - target).magnitude <= absoluteThreshold)
+                return target;
+
+            Vector3 vel = Vector3.zero;
+            return Vector3.SmoothDamp(current, target, ref vel, time);
+        }
+    }
+}
