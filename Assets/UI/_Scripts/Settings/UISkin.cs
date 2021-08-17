a38221cbf67eacf58207ebd39c353d91ab7cb83f
Alex Kovács <kovacsalex95@gmail.com>
2021-08-17 17:08:02 +0200

    UIUtil class separated

--- Assets/UI/_Scripts/Settings/UISkin.cs
+++ Assets/UI/_Scripts/Settings/UISkin.cs
@@ -70,32 +70,3 @@ public class GroupPanelSkin
 {
     public float ElementSpacing = 30;
 }
-
-public static class UIUtil
-{
-    static UISkin uiSkin = null;
-
-    public static UISkin Skin
-    {
-        get {
-            if (uiSkin == null)
-                uiSkin = GameObject.FindObjectOfType<UISkin>();
-
-            return uiSkin;
-        }
-    }
-
-    public static Color ColorOpacity(Color source, float opacity = 1)
-    {
-        return new Color(source.r, source.g, source.b, opacity);
-    }
-
-    public static RectOffset Padding(float left, float right, float top, float bottom)
-    {
-        return new RectOffset(Mathf.RoundToInt(left), Mathf.RoundToInt(right), Mathf.RoundToInt(top), Mathf.RoundToInt(bottom));
-    }
-    public static RectOffset Padding(float all = 0)
-    {
-        return Padding(all, all, all, all);
-    }
-}
