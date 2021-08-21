a45410cb913c9d550ef917b7a9ff14ee829196f7
Alex Kovács <kovacsalex95@gmail.com>
2021-08-21 20:36:28 +0200

    Game phases and modes introduced

--- Assets/Misc/_Scripts/GameController.cs
+++ Assets/Misc/_Scripts/GameController.cs
@@ -8,8 +8,27 @@ public class GameController : MonoBehaviour
 {
     public PlayerInput input { get; private set; }
 
+    public GamePhase Phase { get; private set; }
+    public GameMode Mode { get; private set; }
+
     private void Awake()
     {
         input = GetComponent<PlayerInput>();
+        Phase = GamePhase.WorldSelection;
+        Mode = GameMode.Management;
+    }
+
+    public enum GamePhase
+    {
+        WorldSelection = 0,
+        StartintPoint = 1,
+        Ingame = 2
+    }
+
+    public enum GameMode
+    {
+        Management = 0,
+        Building = 1,
+        Mission = 2
     }
 }
